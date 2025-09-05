acc8e4262b2be9257e5dff7dbb1079355cd4158a
kondaevnil <defox4159@gmail.com>
2025-09-05 23:55:45 +0300

    add calculator && cicd core

---
+++ server/sample_test.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestAplusB(t *testing.T) {
+	if 2+2 != 4 {
+		t.Error("2+2 not equals to 4")
+	}
+}
---
+++ server/сalculations.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"errors"
+	"github.com/Knetic/govaluate"
+	"math"
+	"strings"
+)
+
+var errInvalidInput = errors.New("invalid input")
+
+var functions = map[string]govaluate.ExpressionFunction{
+	"sqrt": func(args ...any) (any, error) {
+		if len(args) != 1 {
+			return nil, errInvalidInput
+		}
+
+		answer, ok := args[0].(float64)
+		if !ok {
+			return nil, errInvalidInput
+		}
+
+		res := math.Sqrt(answer)
+		return res, nil
+	},
+
+	"sin": func(args ...any) (any, error) {
+		if len(args) != 1 {
+			return nil, errInvalidInput
+		}
+
+		answer, ok := args[0].(float64)
+		if !ok {
+			return nil, errInvalidInput
+		}
+
+		res := math.Sin(answer)
+		return res, nil
+	},
+
+	"cos": func(args ...any) (any, error) {
+		if len(args) != 1 {
+			return nil, errInvalidInput
+		}
+
+		answer, ok := args[0].(float64)
+		if !ok {
+			return nil, errInvalidInput
+		}
+
+		res := math.Cos(answer)
+		return res, nil
+	},
+
+	"tg": func(args ...any) (any, error) {
+		if len(args) != 1 {
+			return nil, errInvalidInput
+		}
+		answer, ok := args[0].(float64)
+
+		if !ok {
+			return nil, errInvalidInput
+		}
+
+		res := math.Tan(answer)
+		return res, nil
+	},
+}
+
+func calculate(s string) (float64, error) {
+	expression, err := govaluate.NewEvaluableExpressionWithFunctions(strings.ToLower(s), functions)
+
+	if err != nil {
+		if err.Error() == "Unbalanced parenthesis" { // В случае "2a" он передает плохую ошибку
+			return 0, err
+		}
+		return 0, errInvalidInput
+	}
+
+	result, err := expression.Evaluate(map[string]any{})
+
+	if err != nil {
+		return 0, errInvalidInput
+	}
+
+	answer, ok := result.(float64)
+
+	if !ok || math.IsNaN(answer) || math.IsInf(answer, 1) || math.IsInf(answer, 0) {
+		return 0, errInvalidInput
+	}
+
+	return answer, nil
+}
