3eacceccfecf29ee14aca8c2e4d2bb6a6d54f9b4
iizhukov <iizhukov@icloud.com>
2025-09-07 16:17:48 +0300

    REF: dependencies

--- client/src/shared/config/api.ts
+++ client/src/shared/config/api.ts
@@ -1,6 +1,2 @@
-import dotenv from 'dotenv';
-
-dotenv.config()
-
 export const API_HOST = process.env.REACT_APP_API_HOST
 export const API_BASE_URL = `${process.env.REACT_APP_PROTOCOL}://${API_HOST}`
